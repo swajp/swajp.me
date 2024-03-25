b633aca1b7c19c264a5181ce2abc445126746f9e
swajp <stejskalmira@icloud.com>
2024-03-25 19:37:17 +0100

    better responsivity

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { Button, buttonVariants } from "./ui/button";
+import { Button } from "./ui/button";
 import { Menu, Square } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
+import { usePathname } from "next/navigation";
+import { NAVIGATION } from "@/config";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col  md:flex md:flex-row md:items-center md:justify-between w-full">
@@ -13,44 +20,48 @@ export default function Navbar() {
           <p>swajp</p>
         </Link>
         <div className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:justify-between md:gap-5 md:text-sm lg:gap-6">
-          <Link
-            href="#"
-            className="text-foreground transition-colors hover:text-foreground"
-          >
-            Home
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Projects
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Contact
-          </Link>
-
+          {NAVIGATION.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={cn(
+                "hover:text-foreground",
+                pathname === item.href
+                  ? "text-foreground"
+                  : "text-muted-foreground"
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
           <ModeToggle />
         </div>
       </nav>
       <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
+        <div className="w-full flex md:hidden justify-between">
+          <Link
+            href="#"
+            className="flex items-center gap-2 font-semibold w-fit"
+          >
+            <Square className="size-5 fill-foreground" />
+            <p>swajp</p>
+          </Link>
+          <div>
+            <ModeToggle />
+            <SheetTrigger className="ml-2" asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+          </div>
+        </div>
+        <SheetContent side="right">
           <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="#"
-              className="flex items-center gap-2 font-semibold w-fit"
-            >
-              <Square className="size-5 fill-foreground" />
-              <p>swajp</p>
-            </Link>
             <Link href="#" className="hover:text-foreground">
               Dashboard
             </Link>
