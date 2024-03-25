65e66a8e3fdc336315b3e92a882fba81d9c3d5f1
swajp <stejskalmira@icloud.com>
2024-03-25 17:54:48 +0100

    loading page

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-screen flex items-center gap-2 justify-center">
+      <div className="w-6 h-6 bg-foreground animate-spin" />
+    </div>
+  );
+}
