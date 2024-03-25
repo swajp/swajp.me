7a1038c0152b0750f91d074390e255987bc82e11
swajp <stejskalmira@icloud.com>
2024-03-25 20:47:59 +0100

    moved files

--- app/(root)/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="h-screen flex items-center gap-2 justify-center">
-      <div className="w-6 h-6 bg-foreground animate-spin" />
-    </div>
-  );
-}
