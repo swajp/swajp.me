5c39e7dda0d1f91a535e556523fe46bda4c0d5f1
swajp <stejskalmira@icloud.com>
2024-03-25 19:37:08 +0100

    added config

---
+++ config/index.ts
@@ -0,0 +1,14 @@
+export const NAVIGATION = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
