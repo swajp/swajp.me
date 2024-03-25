76ab8ec1c0ea6ce460029a2b9e50948aa23802a6
swajp <stejskalmira@icloud.com>
2024-03-25 20:46:37 +0100

    edited max width wrapper

--- components/max-width-wrapper.tsx
+++ components/max-width-wrapper.tsx
@@ -9,10 +9,7 @@ export default function MaxWidthWrapper({
 }) {
   return (
     <div
-      className={cn(
-        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
-        className
-      )}
+      className={cn("mx-auto w-full max-w-screen-xl px-6 md:px-20", className)}
     >
       {children}
     </div>
