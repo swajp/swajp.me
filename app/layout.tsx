554ec089787a0a1416f181c2b5913b3b9d56373f
swajp <stejskalmira@icloud.com>
2024-03-31 13:48:32 +0200

    theme fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           storageKey="swajp-theme"
           disableTransitionOnChange
         >
