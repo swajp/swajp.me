2c433c1df88e7fa4c084a29c26ade48648e14905
swajp <stejskalmira@icloud.com>
2024-04-01 20:12:34 +0200

    added cv card

---
+++ components/cv.tsx
@@ -0,0 +1,18 @@
+import { FileText } from "lucide-react";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+export default function CV() {
+  return (
+    <Card className="h-full">
+      <CardHeader>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2 mb-2 text-sm text-muted-foreground">
+            <FileText size={18} />
+            <p>CV</p>
+          </div>
+        </div>
+        <CardTitle>View my CV</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+}
