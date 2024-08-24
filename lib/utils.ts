0b0565f58f878b5da6a74ff6e847c7baa5b22fb0
swajp <stejskalmira@icloud.com>
2024-08-24 16:41:55 +0200

    added new utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Post } from "#site/content"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function formatDate(input: string | number): string {
+    return new Date(input).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    })
+}
+
+export function sortPostsByDate(posts: Array<Post>): Array<Post> {
+    return posts.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime()
+    })
 }
