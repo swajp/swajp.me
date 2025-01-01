a4a23447ef8c54878706b669fa2b7880914e2fa1
Miroslav Stejskal <stejskalmira@icloud.com>
2025-01-01 23:13:55 +0100

    Code format

--- components/coding-hours.tsx
+++ components/coding-hours.tsx
@@ -1,51 +1,42 @@
-"use client";
+"use client"
 
-import { ClockIcon, InfoIcon } from "lucide-react";
-import useSWR from "swr";
+import { ClockIcon, InfoIcon } from "lucide-react"
+import useSWR from "swr"
 
-import { fetcher } from "@/lib/fetcher";
-import { Card, CardHeader, CardTitle } from "./ui/card";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@radix-ui/react-tooltip";
+import { fetcher } from "@/lib/fetcher"
+import { Card, CardHeader, CardTitle } from "./ui/card"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip"
 
 type Wakatime = {
-  seconds: number;
-};
+    seconds: number
+}
 
 const CodingHours = () => {
-  const { data: wakatimeData } = useSWR<Wakatime>("/api/wakatime", fetcher);
+    const { data: wakatimeData } = useSWR<Wakatime>("/api/wakatime", fetcher)
 
-  return (
-    <Card className="h-full group invert">
-      <CardHeader>
-        <div className="flex justify-between">
-          <div className="flex items-center gap-2 mb-2 text-sm">
-            <ClockIcon size={18} />
-            <p>Coding hours</p>
-          </div>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <InfoIcon size={18} />
-              </TooltipTrigger>
-              <TooltipContent className="border p-2 bg-background px-3 rounded-md text-sm text-muted-foreground">
-                I started tracking my coding hours in Mar 13 2024.
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
-        <CardTitle>
-          {wakatimeData
-            ? Math.round(wakatimeData.seconds / 60 / 60) + " hrs"
-            : "No data"}
-        </CardTitle>
-      </CardHeader>
-    </Card>
-  );
-};
+    return (
+        <Card className="h-full group invert">
+            <CardHeader>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-2 mb-2 text-sm">
+                        <ClockIcon size={18} />
+                        <p>Coding hours</p>
+                    </div>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <InfoIcon size={18} />
+                            </TooltipTrigger>
+                            <TooltipContent className="border p-2 bg-background px-3 rounded-md text-sm text-muted-foreground">
+                                I started tracking my coding hours in Mar 13 2024.
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </div>
+                <CardTitle>{wakatimeData ? Math.round(wakatimeData.seconds / 60 / 60) + " hrs" : "No data"}</CardTitle>
+            </CardHeader>
+        </Card>
+    )
+}
 
-export default CodingHours;
+export default CodingHours
