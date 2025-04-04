0e7528fc9f11b5126d4963adc459e739945f8e9c
Miroslav Stejskal <me@swajp.me>
2025-04-04 19:38:00 +0200

    Refactor Wakatime API route to handle missing API key and improve code formatting

--- app/api/wakatime/route.ts
+++ app/api/wakatime/route.ts
@@ -1,27 +1,31 @@
-import { unstable_noStore as noStore } from "next/cache";
-import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache"
+import { NextResponse } from "next/server"
 
-export const runtime = "edge";
+export const runtime = "edge"
 
 export const GET = async () => {
-  noStore();
+    noStore()
 
-  const res = await fetch(
-    "https://wakatime.com/api/v1/users/current/all_time_since_today",
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          process.env.WAKATIME_API_KEY || ""
-        ).toString("base64")}`,
-      },
+    if (!process.env.WAKATIME_API_KEY) {
+        return NextResponse.json(
+            {
+                error: "WAKATIME_API_KEY is not set in the environment variables"
+            },
+            { status: 500 }
+        )
     }
-  );
 
-  const {
-    data: { total_seconds },
-  } = await res.json();
+    const res = await fetch("https://wakatime.com/api/v1/users/current/all_time_since_today", {
+        headers: {
+            Authorization: `Basic ${Buffer.from(process.env.WAKATIME_API_KEY || "").toString("base64")}`
+        }
+    })
 
-  return NextResponse.json({
-    seconds: total_seconds,
-  });
-};
+    const {
+        data: { total_seconds }
+    } = await res.json()
+
+    return NextResponse.json({
+        seconds: total_seconds
+    })
+}
